5c22138e2613f399f617640c7cbc9ece4ad886cb
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2024-07-31 11:25:38 +0100

    fix: remove redundant styles and make the navbar a nav element
    Co-authored-by: IlliaDerevianko <IlliaDerevianko@users.noreply.github.com>
    Co-authored-by: Matthew Alex <matalex412@gmail.com>
    Co-authored-by: nick-bolas <nick-bolas@users.noreply.github.com>

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,63 +10,53 @@ import React from "react"
 const Navbar = async () => {
   const session = await auth()
   return (
-    <Flex className={styles.container} justify="between">
-      <Flex gap="9" wrap="wrap">
-        <Link href="/">
-          <Flex className={styles.logosContainer}>
-            <Image
-              src="images/cpp-connect-logo.svg"
-              alt="cpp connect logo"
-              width={0}
-              height={0}
-              style={{ aspectRatio: "100/75" }}
-            />
-            <Image
-              src="images/imperial-logo.svg"
-              alt="imperial logo"
-              width={0}
-              height={0}
-              style={{ aspectRatio: "213/66" }}
-            />
-          </Flex>
-        </Link>
+    <Flex className={styles.container} justify="between" asChild>
+      <nav>
+        <Flex gap="9" wrap="wrap">
+          <Link href="/">
+            <Flex className={styles.logosContainer}>
+              <Image src="images/cpp-connect-logo.svg" alt="cpp connect logo" width={0} height={0} />
+              <Image src="images/imperial-logo.svg" alt="imperial logo" width={0} height={0} />
+            </Flex>
+          </Link>
 
-        {session?.user && (
-          <Flex className={styles.linksContainer}>
-            <Link href="/" className={styles.link}>
-              <span>Companies</span>
-            </Link>
-            <Link href="/" className={styles.link}>
-              <span>Events</span>
-            </Link>
-            <Link href="/" className={styles.link}>
-              <span>Opportunities</span>
-            </Link>
-            <Link href="/" className={styles.link}>
-              <span>Students</span>
-            </Link>
-          </Flex>
+          {session?.user && (
+            <Flex className={styles.linksContainer}>
+              <Link href="/" className={styles.link}>
+                <span>Companies</span>
+              </Link>
+              <Link href="/" className={styles.link}>
+                <span>Events</span>
+              </Link>
+              <Link href="/" className={styles.link}>
+                <span>Opportunities</span>
+              </Link>
+              <Link href="/" className={styles.link}>
+                <span>Students</span>
+              </Link>
+            </Flex>
+          )}
+        </Flex>
+
+        {session?.user ? (
+          <Avatar.Root className={styles.AvatarRoot}>
+            <Avatar.Image src={session.user.image ?? undefined} alt="Profile" className={styles.AvatarImage} />
+            <Avatar.Fallback className={styles.AvatarFallback}>
+              {session.user.name
+                ?.split(",")
+                .reverse()
+                .join(" ")
+                .split(/\s|\-/g)
+                .map(name => name[0]?.toUpperCase())
+                .join("")}
+            </Avatar.Fallback>
+          </Avatar.Root>
+        ) : (
+          <Link href="/login" className={styles.link}>
+            <span>Log In</span>
+          </Link>
         )}
-      </Flex>
-
-      {session?.user ? (
-        <Avatar.Root className={styles.AvatarRoot}>
-          <Avatar.Image src={session.user.image ?? undefined} alt="Profile" className={styles.AvatarImage} />
-          <Avatar.Fallback className={styles.AvatarFallback}>
-            {session.user.name
-              ?.split(",")
-              .reverse()
-              .join(" ")
-              .split(/\s|\-/g)
-              .map(name => name[0]?.toUpperCase())
-              .join("")}
-          </Avatar.Fallback>
-        </Avatar.Root>
-      ) : (
-        <Link href="/login" className={styles.link}>
-          <span>Log In</span>
-        </Link>
-      )}
+      </nav>
     </Flex>
   )
 }
