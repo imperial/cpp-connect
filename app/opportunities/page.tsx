d22577baffe293bc52144a29d9cfa06293e5170a
Illia Derevianko <iliaderevianko30@gmail.com>
2024-07-31 18:56:41 +0300

    feat: add Table for opportunities

--- app/opportunities/page.tsx
+++ app/opportunities/page.tsx
@@ -1,5 +1,7 @@
-import prisma from '@/lib/db'
-import React from 'react'
+import ListingTable from "@/components/Table"
+import prisma from "@/lib/db"
+
+import React from "react"
 
 const OpportunitiesPage = async () => {
   const opportunities = await prisma.opportunity.findMany({
@@ -7,12 +9,10 @@ const OpportunitiesPage = async () => {
       company: true,
     },
   })
-  
+
   return (
     <div>
-        {opportunities.map((opportunity, index) => (
-            <p key={index}>{opportunity.company.id}</p>
-        ))}
+      <ListingTable opportunities={opportunities} />
     </div>
   )
 }
---
+++ components/Table.tsx
@@ -0,0 +1,39 @@
+import type { Company, Opportunity } from "@prisma/client"
+import { Table } from "@radix-ui/themes"
+import React from "react"
+
+interface ListingTableProps {
+  opportunities: ({ company: Company } & Opportunity)[]
+}
+
+const ListingTable = ({ opportunities }: ListingTableProps) => {
+  return (
+    <Table.Root size="3">
+      <Table.Header>
+        <Table.Row>
+          <Table.ColumnHeaderCell>Company</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Position</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Location</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Type</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Posted</Table.ColumnHeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        {opportunities
+          .filter(opportunity => opportunity.available)
+          .map((opportunity, index) => (
+            <Table.Row key={index}>
+              <Table.RowHeaderCell>{opportunity.company.id}</Table.RowHeaderCell>
+              <Table.Cell>{opportunity.position}</Table.Cell>
+              <Table.Cell>{opportunity.location}</Table.Cell>
+              <Table.Cell>{opportunity.type}</Table.Cell>
+              <Table.Cell>{opportunity.createdAt.toLocaleDateString()}</Table.Cell>
+            </Table.Row>
+          ))}
+      </Table.Body>
+    </Table.Root>
+  )
+}
+
+export default ListingTable
