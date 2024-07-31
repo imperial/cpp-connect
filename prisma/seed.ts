bd52ccd7e47214ba1293fa69831f5c38476d56b7
Illia Derevianko <iliaderevianko30@gmail.com>
2024-07-31 17:52:29 +0300

    feat: add dummy opportunities data using seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,14 +3,14 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  const alice = await prisma.user.upsert({
+  await prisma.user.upsert({
     where: { email: 'ab1223@ic.ac.uk' },
     update: {},
     create: {
       email: 'ab1223@ic.ac.uk',
     },
   })
-  const bob = await prisma.user.upsert({
+  await prisma.user.upsert({
     where: { email: 'ma4723@ic.ac.uk' },
     update: {},
     create: {
@@ -18,9 +18,39 @@ async function main() {
     },
   })
 
-  const users = await prisma.user.findMany()
-  console.log(users)
-  
+  await prisma.company.upsert({
+    where: { id: "1234"},
+    update: {},
+    create: { id: "1234" },
+  })
+
+  await prisma.opportunity.upsert({
+    where: { opportunityID: "1"},
+    update: {},
+    create: { opportunityID: "1",
+      position: "software engineer",
+      location: "london",
+      available: true,
+      type: "internship",
+      companyID: "1234"
+    }
+  })
+
+  await prisma.opportunity.upsert({
+    where: { opportunityID: "2"},
+    update: {},
+    create: { opportunityID: "2",
+      position: "prompt engineer",
+      location: "mars",
+      available: false,
+      type: "job",
+      companyID: "1234"
+    }
+  })
+
+  const opportunities = await prisma.opportunity.findMany()
+  console.log(opportunities)
+
 }
 
 main()
