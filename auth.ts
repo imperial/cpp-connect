84f0751883052f4c2e653dde3232adb34c94384f
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-07-31 16:54:00 +0100

    wip: add roles to the DB & try to authenticate on them

--- auth.ts
+++ auth.ts
@@ -1,25 +1,53 @@
-import NextAuth, { NextAuthConfig } from "next-auth"
+import prisma from "@/lib/db"
+
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { Role } from "@prisma/client"
+import NextAuth, { DefaultSession, NextAuthConfig, User } from "next-auth"
 import AzureAd from "next-auth/providers/azure-ad"
 
-export const config: NextAuthConfig = {
-    providers: [
-        AzureAd({
-            clientId: process.env.AZURE_AD_CLIENT_ID,
-            clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
-            tenantId: process.env.AZURE_AD_TENANT_ID,
-        }),
-    ],
-    callbacks: {
-        authorized: async ({ auth }) => {
-            // Logged in users are authenticated, otherwise redirect to login page
-            return !!auth
-        },
-    },
+declare module "@auth/core/adapters" {
+  interface AdapterUser {
+    role?: Role
+  }
+}
+declare module "next-auth" {
+  interface Session {
+    user: {
+      role: Role
+    } & DefaultSession["user"]
+  }
+}
 
-    // TODO: uncomment when we have a login page
-    // pages: {
-    //   signIn: "/login"
-    // },
+export const config: NextAuthConfig = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    AzureAd({
+      clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+      tenantId: process.env.AZURE_AD_TENANT_ID,
+      profile(profile) {
+        return { role: profile.role ?? "STUDENT", ...profile }
+      },
+    }),
+  ],
+  callbacks: {
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth
+    },
+    session({ session, user }) {
+      if (!user?.role) {
+        throw new Error("Role not found in user when creating session")
+      }
+      session.user.role = user.role
+      return session
+    },
+  },
+
+  // TODO: uncomment when we have a login page
+  // pages: {
+  //   signIn: "/login"
+  // },
 }
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config)
