d1b6bb4087a568fa01d985384b06ec9401369289
Illia Derevianko <iliaderevianko30@gmail.com>
2024-08-01 19:01:18 +0300

    wip: company page styles

--- app/company/[id]/page.tsx
+++ app/company/[id]/page.tsx
@@ -1,47 +1,50 @@
-import React from 'react'
 import prisma from "@/lib/db"
-import { notFound } from 'next/navigation';
-import { Flex, Text } from '@radix-ui/themes';
+
+import styles from "./page.module.scss"
+
+import { Flex, Text } from "@radix-ui/themes"
+import { notFound } from "next/navigation"
+import React from "react"
 
 const CompanyPage = async ({ params }: { params: { id: string } }) => {
-  const companyProfile = await prisma.companyProfile.findUnique({where: {id: parseInt(params.id)}})
+  const companyProfile = await prisma.companyProfile.findUnique({ where: { id: parseInt(params.id) } })
 
   if (!companyProfile) {
     notFound()
   }
 
   return (
-    <Flex direction="column">
+    <Flex gap="5" direction="column">
       <Flex>
         <Text>{companyProfile.name}</Text>
       </Flex>
-      <Flex>
-        <Text>Summary</Text>
-      <Text>{companyProfile.summary}</Text>
+      <Flex direction="column">
+        <Text className={styles.subheading}>Summary</Text>
+        <Text>{companyProfile.summary}</Text>
       </Flex>
       <Flex direction="row" wrap="wrap">
-        <Flex >
-          <Text>Sector</Text>
+        <Flex>
+          <Text className={styles.subheading}>Sector</Text>
           <Text>{companyProfile.sector}</Text>
         </Flex>
-        <Flex >
-          <Text>Size</Text>
+        <Flex>
+          <Text className={styles.subheading}>Size</Text>
           <Text>{companyProfile.size ?? "N/A"}</Text>
         </Flex>
-        <Flex >
-          <Text>HQ</Text>
+        <Flex>
+          <Text className={styles.subheading}>HQ</Text>
           <Text>{companyProfile.hq ?? "N/A"}</Text>
         </Flex>
-        <Flex >
+        {/* <Flex>
           <Text>Founded</Text>
           <Text>{companyProfile.founded ?? "N/A"}</Text>
         </Flex>
-        <Flex >
+        <Flex>
           <Text>Contacts</Text>
           <Text>{companyProfile.website}</Text>
           <Text>{companyProfile.email ?? "N/A"}</Text>
           <Text>{companyProfile.phone ?? "N/A"}</Text>
-        </Flex>
+        </Flex> */}
       </Flex>
     </Flex>
   )
