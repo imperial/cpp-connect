6fb3f6c95ef5d730e9e640ac200365965ea21b6d
Illia Derevianko <iliaderevianko30@gmail.com>
2024-08-01 16:03:12 +0300

    refactor: use date-fns instead of a custom function for relative dates

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,31 +1,10 @@
 import type { Company, Opportunity } from "@prisma/client"
 import { Table } from "@radix-ui/themes"
+import { formatDistanceToNowStrict } from "date-fns"
 import React from "react"
 
 interface ListingTableProps {
   opportunities: ({ company: Company } & Opportunity)[]
-}
-
-const formatDate = (date: Date): string => {
-  const MILLIS_PER_MINUTE = 60000
-  const MILLIS_PER_HOUR = 3600000
-  const MILLIS_PER_DAY = 86400000
-  const MILLIS_PER_WEEK = 604800000
-
-  const currentDate = new Date()
-  // get difference in milliseconds
-  const difference = currentDate.getTime() - date.getTime()
-  if (difference < MILLIS_PER_MINUTE) {
-    return "Just now"
-  } else if (difference < MILLIS_PER_HOUR) {
-    return `${Math.floor(difference / MILLIS_PER_MINUTE)} minutes ago`
-  } else if (difference < MILLIS_PER_DAY) {
-    return `${Math.floor(difference / MILLIS_PER_HOUR)} hours ago`
-  } else if (difference < MILLIS_PER_WEEK) {
-    return `${Math.floor(difference / MILLIS_PER_DAY)} days ago`
-  } else {
-    return date.toLocaleDateString()
-  }
 }
 
 const ListingTable = ({ opportunities }: ListingTableProps) => {
@@ -50,7 +29,7 @@ const ListingTable = ({ opportunities }: ListingTableProps) => {
               <Table.Cell>{opportunity.position}</Table.Cell>
               <Table.Cell>{opportunity.location}</Table.Cell>
               <Table.Cell>{opportunity.type}</Table.Cell>
-              <Table.Cell>{formatDate(opportunity.createdAt)}</Table.Cell>
+              <Table.Cell>{formatDistanceToNowStrict(opportunity.createdAt, { addSuffix: true })}</Table.Cell>
             </Table.Row>
           ))}
       </Table.Body>
