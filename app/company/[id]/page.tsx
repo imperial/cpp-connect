ec883705fa865784fa0821b615f7e036f525e2c5
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2024-08-05 12:26:48 +0100

    feat: add contact details to company profile card, refactor company detail to component
    Co-authored-by: Matthew Alex <matalex412@gmail.com>

--- app/company/[id]/page.tsx
+++ app/company/[id]/page.tsx
@@ -7,7 +7,28 @@ import { Box, Card, Flex, Heading, Inset, Link, Text } from "@radix-ui/themes"
 import Image from "next/image"
 import { notFound } from "next/navigation"
 import React from "react"
+import { BsEnvelope, BsGlobe, BsTelephone } from "react-icons/bs"
 import Markdown from "react-markdown"
+
+/**
+ * Conditional rendering of company detail. Will only render if children are truthy.
+ * @param title The title of the company detail.
+ * @param children The company detail provided.
+ */
+const CompanyDetail = ({ title, children }: { title: string; children: React.ReactNode }) => {
+  if (children) {
+    return (
+      <>
+        <Heading size="4" as="h2" mt="4">
+          {title}
+        </Heading>
+        {children}
+      </>
+    )
+  } else {
+    return null
+  }
+}
 
 const CompanyPage = async ({ params }: { params: { id: string } }) => {
   const companyProfile = await prisma.companyProfile.findUnique({ where: { id: parseInt(params.id) } })
@@ -20,18 +41,24 @@ const CompanyPage = async ({ params }: { params: { id: string } }) => {
     <Flex gap="3" direction="column">
       <Card className={styles.headerCard}>
         <Inset clip="padding-box" p="0" side="top">
-          <Image src="/images/amazon-logo.svg" alt={"amazon"} width={0} height={0} className={styles.banner} />
+          <Image
+            src={companyProfile.logo}
+            alt={`${companyProfile.name} banner`}
+            width={0}
+            height={0}
+            className={styles.banner}
+          />
         </Inset>
 
         <Flex direction="column">
-          <Flex gap="5" direction="column" className={styles.companyInfo} p="4" pt="0">
+          <Flex gap="3" direction="column" className={styles.companyInfo} p="4" pt="0">
             <Box className={styles.companyLogoContainer}>
               <Card className={styles.companyLogo}>
-                <Image src="/images/amazon-logo.svg" alt={"amazon"} width={0} height={0} />
+                <Image src={companyProfile.logo} alt={`${companyProfile.name} logo`} width={0} height={0} />
               </Card>
             </Box>
 
-            <Heading color="blue" size="7">
+            <Heading color="blue" size="7" mt="4">
               {companyProfile.name}
             </Heading>
 
@@ -40,53 +67,52 @@ const CompanyPage = async ({ params }: { params: { id: string } }) => {
                 .filter(x => x)
                 .join(" • ")}
             </Text>
+
+            <Flex align="center" gap="2">
+              <BsGlobe />
+              <Link href={companyProfile.website} target="_blank" className={styles.contactLink}>
+                {companyProfile.website}
+              </Link>
+            </Flex>
+
+            {companyProfile.email && (
+              <Flex align="center" gap="2">
+                <BsEnvelope />
+                <Link href={`mailto:${companyProfile.email}`} className={styles.contactLink}>
+                  {companyProfile.email}
+                </Link>
+              </Flex>
+            )}
+
+            {companyProfile.email && (
+              <Flex align="center" gap="2">
+                <BsTelephone />
+                <Text>{companyProfile.phone}</Text>
+              </Flex>
+            )}
           </Flex>
         </Flex>
       </Card>
+
       <Card className={styles.aboutCard}>
         <Collapsible.Root className={styles.CollapsibleRoot}>
           <Heading size="6">About</Heading>
           <Box className={styles.summaryContainer}>
             <Markdown className={styles.markdownContainer}>{companyProfile.summary}</Markdown>
 
-            <Heading size="4" as="h2" mt="4">
-              Website
-            </Heading>
-            <Link href={companyProfile.website} target="_blank">
-              {companyProfile.website}
-            </Link>
+            <CompanyDetail title="Website">
+              <Link href={companyProfile.website} target="_blank">
+                {companyProfile.website}
+              </Link>
+            </CompanyDetail>
 
-            <Heading size="4" as="h2" mt="4">
-              Sector
-            </Heading>
-            {companyProfile.sector}
+            <CompanyDetail title="Sector">{companyProfile.sector}</CompanyDetail>
 
-            {companyProfile.hq && (
-              <>
-                <Heading size="4" as="h2" mt="4">
-                  Headquarters
-                </Heading>
-                {companyProfile.hq}
-              </>
-            )}
+            <CompanyDetail title="Headquarters">{companyProfile.hq}</CompanyDetail>
 
-            {companyProfile.founded && (
-              <>
-                <Heading size="4" as="h2" mt="4">
-                  Founded
-                </Heading>
-                {companyProfile.founded}
-              </>
-            )}
+            <CompanyDetail title="Founded">{companyProfile.founded}</CompanyDetail>
 
-            {companyProfile.size && (
-              <>
-                <Heading size="4" as="h2" mt="4">
-                  Size
-                </Heading>
-                {companyProfile.size}
-              </>
-            )}
+            <CompanyDetail title="Size">{companyProfile.size}</CompanyDetail>
           </Box>
           <Collapsible.Trigger className={styles.CollapsibleTrigger} asChild>
             <Text size="2">Read more...</Text>
