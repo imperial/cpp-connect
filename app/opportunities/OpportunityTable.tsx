99c7057cb53122edb4ac44d1f37fc0999cd98a4e
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-08-05 13:57:35 +0100

    feat: add pagination

--- app/opportunities/OpportunityTable.tsx
+++ app/opportunities/OpportunityTable.tsx
@@ -3,7 +3,14 @@
 import styles from "./opportunities.module.scss"
 
 import type { CompanyProfile, Opportunity } from "@prisma/client"
-import { ChevronDownIcon, ChevronUpIcon } from "@radix-ui/react-icons"
+import {
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronUpIcon,
+  DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
+} from "@radix-ui/react-icons"
 import { Box, Button, Flex, IconButton, Table } from "@radix-ui/themes"
 import {
   ColumnFiltersState,
@@ -13,12 +20,15 @@ import {
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
+  getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table"
 import { info } from "console"
 import { formatDistanceToNowStrict } from "date-fns"
 import React, { useState } from "react"
+import { Pagination } from "react-headless-pagination"
+import ReactPaginate from "react-paginate"
 
 const ICON_SIZE = 20
 
@@ -75,57 +85,98 @@ const getSortingIcon = (isSorted: false | SortDirection): React.ReactNode => {
 const ListingTable = ({ opportunities }: ListingTableProps) => {
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
   const [sorting, setSorting] = useState<SortingState>([])
+  const [pagination, setPagination] = useState({
+    pageIndex: 0, //initial page index
+    pageSize: 15, //default page size
+  })
   const table = useReactTable({
     data: opportunities,
     columns,
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
-    state: { columnFilters, sorting },
+    state: { columnFilters, sorting, pagination },
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onPaginationChange: setPagination,
   })
 
   return (
-    <Table.Root size="3">
-      <Table.Header>
-        {table.getHeaderGroups().map(headerGroup => (
-          <Table.Row key={headerGroup.id}>
-            {headerGroup.headers.map(header => (
-              <Table.ColumnHeaderCell
-                key={header.id}
-                onClick={() => header.column.toggleSorting(header.column.getIsSorted() === "asc")}
-                className={styles.tableHeader}
-              >
-                <Flex align="center">
-                  <span style={{ flex: "1 1 0" }}>
-                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                  </span>
-                  {getSortingIcon(header.column.getIsSorted())}
-                </Flex>
-              </Table.ColumnHeaderCell>
-            ))}
-          </Table.Row>
-        ))}
-      </Table.Header>
+    <Flex gap="4" direction="column">
+      <Table.Root size="3">
+        <Table.Header>
+          {table.getHeaderGroups().map(headerGroup => (
+            <Table.Row key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <Table.ColumnHeaderCell
+                  key={header.id}
+                  onClick={() => header.column.toggleSorting(header.column.getIsSorted() === "asc")}
+                  className={styles.tableHeader}
+                >
+                  <Flex align="center">
+                    <span style={{ flex: "1 1 0" }}>
+                      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                    </span>
+                    {getSortingIcon(header.column.getIsSorted())}
+                  </Flex>
+                </Table.ColumnHeaderCell>
+              ))}
+            </Table.Row>
+          ))}
+        </Table.Header>
 
-      <Table.Body>
-        {table.getRowModel().rows.map(row => (
-          <Table.Row key={row.id}>
-            {row
-              .getVisibleCells()
-              .map(cell =>
-                cell.column.getIsFirstColumn() ? (
-                  <Table.RowHeaderCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </Table.RowHeaderCell>
-                ) : (
-                  <Table.Cell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</Table.Cell>
-                ),
-              )}
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table.Root>
+        <Table.Body>
+          {table.getRowModel().rows.map(row => (
+            <Table.Row key={row.id}>
+              {row
+                .getVisibleCells()
+                .map(cell =>
+                  cell.column.getIsFirstColumn() ? (
+                    <Table.RowHeaderCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </Table.RowHeaderCell>
+                  ) : (
+                    <Table.Cell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</Table.Cell>
+                  ),
+                )}
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+      <Pagination
+        totalPages={table.getPageCount()}
+        middlePagesSiblingCount={2}
+        edgePageCount={0}
+        currentPage={table.getState().pagination.pageIndex}
+        setCurrentPage={table.setPageIndex}
+        className=""
+        truncableText="..."
+        truncableClassName=""
+      >
+        <nav className={styles.tablePagination}>
+          <ul>
+            <IconButton variant="outline" disabled={!table.getCanPreviousPage()} onClick={table.firstPage}>
+              <DoubleArrowLeftIcon />
+            </IconButton>
+            <Pagination.PrevButton className="" as={<IconButton variant="outline" />}>
+              <ChevronLeftIcon />
+            </Pagination.PrevButton>
+            <Pagination.PageButton
+              as={<Button variant="outline" />}
+              activeClassName={styles.activePage}
+              inactiveClassName=""
+              className=""
+            />
+            <Pagination.NextButton className="" as={<IconButton variant="outline" />}>
+              <ChevronRightIcon />
+            </Pagination.NextButton>
+            <IconButton variant="outline" disabled={!table.getCanNextPage()} onClick={table.lastPage}>
+              <DoubleArrowRightIcon />
+            </IconButton>
+          </ul>
+        </nav>
+      </Pagination>
+    </Flex>
   )
 }
 
