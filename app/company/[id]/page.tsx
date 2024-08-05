969a1e487fffeb350f969b360a759f6d619c161d
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2024-08-05 11:52:27 +0100

    feat: support markdown for company summary, update about card
    Co-authored-by: Matthew Alex <matalex412@gmail.com>

--- app/company/[id]/page.tsx
+++ app/company/[id]/page.tsx
@@ -3,10 +3,11 @@ import prisma from "@/lib/db"
 import styles from "./page.module.scss"
 
 import * as Collapsible from "@radix-ui/react-collapsible"
-import { Box, Card, Flex, Heading, Inset, Text } from "@radix-ui/themes"
+import { Box, Card, Flex, Heading, Inset, Link, Text } from "@radix-ui/themes"
 import Image from "next/image"
 import { notFound } from "next/navigation"
 import React from "react"
+import Markdown from "react-markdown"
 
 const CompanyPage = async ({ params }: { params: { id: string } }) => {
   const companyProfile = await prisma.companyProfile.findUnique({ where: { id: parseInt(params.id) } })
@@ -16,42 +17,80 @@ const CompanyPage = async ({ params }: { params: { id: string } }) => {
   }
 
   return (
-    <Flex gap="1" direction="column">
+    <Flex gap="3" direction="column">
       <Card className={styles.headerCard}>
-        <Inset clip="padding-box" p="0" side="top" pb="current">
+        <Inset clip="padding-box" p="0" side="top">
           <Image src="/images/amazon-logo.svg" alt={"amazon"} width={0} height={0} className={styles.banner} />
         </Inset>
+
         <Flex direction="column">
-          <Flex gap="5" direction="column" className={styles.companyInfo}>
+          <Flex gap="5" direction="column" className={styles.companyInfo} p="4" pt="0">
             <Box className={styles.companyLogoContainer}>
               <Card className={styles.companyLogo}>
                 <Image src="/images/amazon-logo.svg" alt={"amazon"} width={0} height={0} />
               </Card>
             </Box>
+
             <Heading color="blue" size="7">
               {companyProfile.name}
             </Heading>
-            <Text size="5">{companyProfile.sector}</Text>
-            <Text>{[companyProfile.hq, companyProfile.founded, companyProfile.size].filter(x => x).join(" • ")}</Text>
+
+            <Text>
+              {[companyProfile.sector, companyProfile.hq, companyProfile.size && `${companyProfile.size} employees`]
+                .filter(x => x)
+                .join(" • ")}
+            </Text>
           </Flex>
         </Flex>
       </Card>
-      <Card>
+      <Card className={styles.aboutCard}>
         <Collapsible.Root className={styles.CollapsibleRoot}>
-          <Collapsible.Trigger>
-            <Heading size="6">About</Heading>
-          </Collapsible.Trigger>
+          <Heading size="6">About</Heading>
           <Box className={styles.summaryContainer}>
-            <Text>{companyProfile.summary}</Text>
-            Founded in {companyProfile.founded}
-            <br />
-            Located in {companyProfile.hq}
-            <br />
-            {companyProfile.size} employees
-            <br />
+            <Markdown className={styles.markdownContainer}>{companyProfile.summary}</Markdown>
+
+            <Heading size="4" as="h2" mt="4">
+              Website
+            </Heading>
+            <Link href={companyProfile.website} target="_blank">
+              {companyProfile.website}
+            </Link>
+
+            <Heading size="4" as="h2" mt="4">
+              Sector
+            </Heading>
             {companyProfile.sector}
-            <br />
+
+            {companyProfile.hq && (
+              <>
+                <Heading size="4" as="h2" mt="4">
+                  Headquarters
+                </Heading>
+                {companyProfile.hq}
+              </>
+            )}
+
+            {companyProfile.founded && (
+              <>
+                <Heading size="4" as="h2" mt="4">
+                  Founded
+                </Heading>
+                {companyProfile.founded}
+              </>
+            )}
+
+            {companyProfile.size && (
+              <>
+                <Heading size="4" as="h2" mt="4">
+                  Size
+                </Heading>
+                {companyProfile.size}
+              </>
+            )}
           </Box>
+          <Collapsible.Trigger className={styles.CollapsibleTrigger} asChild>
+            <Text size="2">Read more...</Text>
+          </Collapsible.Trigger>
         </Collapsible.Root>
       </Card>
     </Flex>
