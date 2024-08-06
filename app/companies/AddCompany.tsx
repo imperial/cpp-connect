3e7659ca4b18daeb4742bbf9addfa7975452e3fe
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-08-06 17:15:59 +0100

    feat: add company form scaffold
    Co-authored-by: IlliaDerevianko <IlliaDerevianko@users.noreply.github.com>

--- app/companies/AddCompany.tsx
+++ app/companies/AddCompany.tsx
@@ -1,3 +1,5 @@
+import AdminOnlyArea from "@/components/rbac/AdminOnlyArea"
+
 import { ExclamationTriangleIcon, PlusIcon } from "@radix-ui/react-icons"
 import { Button, Callout, Dialog, Flex, Text, TextField } from "@radix-ui/themes"
 import React from "react"
@@ -17,37 +19,44 @@ export const AddCompany = () => {
           Add a new company profile
         </Dialog.Description>
 
-        <Flex direction="column" gap="3">
-          <Callout.Root>
-            <Callout.Icon>
-              <ExclamationTriangleIcon />
-            </Callout.Icon>
-            <Callout.Text>You can add company users & other details after this step.</Callout.Text>
-          </Callout.Root>
-          <label>
-            <Text as="div" size="2" mb="1" weight="bold">
-              Name
-            </Text>
-            <TextField.Root defaultValue="Freja Johnsen" placeholder="Enter your full name" />
-          </label>
-          <label>
-            <Text as="div" size="2" mb="1" weight="bold">
-              Email
-            </Text>
-            <TextField.Root defaultValue="freja@example.com" placeholder="Enter your email" />
-          </label>
-        </Flex>
-
-        <Flex gap="3" mt="4" justify="end">
-          <Dialog.Close>
-            <Button variant="soft" color="gray">
-              Cancel
-            </Button>
-          </Dialog.Close>
-          <Dialog.Close>
-            <Button>Save</Button>
-          </Dialog.Close>
-        </Flex>
+        <AdminOnlyArea>
+          <Flex direction="column" gap="3">
+            <Callout.Root>
+              <Callout.Icon>
+                <ExclamationTriangleIcon />
+              </Callout.Icon>
+              <Callout.Text>You can add company users & other details after this step.</Callout.Text>
+            </Callout.Root>
+            <label>
+              <Text as="div" size="2" mb="1" weight="bold">
+                Company name
+              </Text>
+              <TextField.Root placeholder="E.g., Imperial" />
+            </label>
+            <label>
+              <Text as="div" size="2" mb="1" weight="bold">
+                Website
+              </Text>
+              <TextField.Root placeholder="E.g., https://imperial.ic.ac.uk" />
+            </label>
+            <label>
+              <Text as="div" size="2" mb="1" weight="bold">
+                Sector
+              </Text>
+              <TextField.Root placeholder="E.g., Education" />
+            </label>
+          </Flex>
+          <Flex gap="3" mt="4" justify="end">
+            <Dialog.Close>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </Dialog.Close>
+            <Dialog.Close>
+              <Button>Save</Button>
+            </Dialog.Close>
+          </Flex>
+        </AdminOnlyArea>
       </Dialog.Content>
     </Dialog.Root>
   )
--- app/companies/CompanyAdminActions.tsx
+++ app/companies/CompanyAdminActions.tsx
@@ -1,21 +1,23 @@
 import DisplayOnlyIfAdmin from "@/components/rbac/DisplayOnlyIfAdmin"
+
+import { AddCompany } from "./AddCompany"
+import styles from "./company-admin-actions.module.scss"
+
 import { PlusIcon } from "@radix-ui/react-icons"
 import { Button, Card, Flex, Heading, Text } from "@radix-ui/themes"
-import React from 'react'
-import styles from "./company-admin-actions.module.scss"
-import { AddCompany } from "./AddCompany"
+import React from "react"
 
 export const CompanyAdminActions = () => {
-	return (
-		<DisplayOnlyIfAdmin>
-			<Card variant="surface" className={styles.adminToolbar}>
-				<Flex gap="3" direction="row" align="center" justify="between" p="2">
-					<Heading size="6">Admin Actions</Heading>
-					<Flex gap="3" direction="row" align="center">
-						<AddCompany />
-					</Flex>
-				</Flex>
-			</Card>
-		</DisplayOnlyIfAdmin>
-	)
+  return (
+    <DisplayOnlyIfAdmin>
+      <Card variant="surface" className={styles.adminToolbar}>
+        <Flex gap="3" direction="row" align="center" justify="between" p="2">
+          <Heading size="6">Admin Actions</Heading>
+          <Flex gap="3" direction="row" align="center">
+            <AddCompany />
+          </Flex>
+        </Flex>
+      </Card>
+    </DisplayOnlyIfAdmin>
+  )
 }
