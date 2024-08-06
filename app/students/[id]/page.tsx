8db24f98ab31b973b2589e17f3b8c9b83bcfbe8f
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2024-08-06 17:23:11 +0100

    style: add styles to student profile page
    Co-authored-by: Matthew Alex <matalex412@gmail.com>

--- app/students/[id]/page.tsx
+++ app/students/[id]/page.tsx
@@ -5,7 +5,7 @@ import prisma from "@/lib/db"
 
 import styles from "./page.module.scss"
 
-import { Flex, Heading, Link, Text } from "@radix-ui/themes"
+import { Box, Card, Flex, Heading, Link, Text } from "@radix-ui/themes"
 import { format } from "date-fns"
 import { notFound } from "next/navigation"
 import React from "react"
@@ -41,97 +41,103 @@ const StudentProfilePage = async ({ params }: { params: { id: string } }) => {
 
   return (
     <StudentOnlyArea>
-      <Flex gap="3" direction="row" style={{ border: "solid black 1px" }}>
-        <Flex direction="column" align="center" gap="5">
-          <UserAvatar user={studentProfile.user} size={10} />
+      <Flex gap="3" direction="row" wrap="wrap">
+        <Card variant="ghost" className={styles.shortDetailsCard}>
+          <Flex direction="column" align="center" gap="5">
+            <UserAvatar user={studentProfile.user} size={10} />
 
-          <Flex align="center" gap="2">
-            <Flex direction="column" align="center">
-              <Heading size="7">{studentProfile.user.name}</Heading>
-              <Text color="gray" size="1">
-                Last updated: {format(studentProfile.updatedAt, "dd/MM/yyyy")}
-              </Text>
+            <Flex align="center" gap="2">
+              <Flex direction="column" align="center">
+                <Heading size="7">{studentProfile.user.name}</Heading>
+                <Text color="gray" size="1">
+                  Last updated: {format(studentProfile.updatedAt, "dd/MM/yyyy")}
+                </Text>
+              </Flex>
+
+              {studentProfile.cv && (
+                <Link href={studentProfile.cv} target="_blank" mb={"0"}>
+                  <BsFileEarmarkText size="35" title="download cv" color="black" />
+                </Link>
+              )}
             </Flex>
 
-            {studentProfile.cv && (
-              <Link href={studentProfile.cv} target="_blank">
-                <BsFileEarmarkText size="35" title="download cv" />
-              </Link>
-            )}
-          </Flex>
+            {studentProfile.course && <Text>{studentProfile.course}</Text>}
 
-          {studentProfile.course && <Text>{studentProfile.course}</Text>}
+            <Box>
+              {studentProfile.lookingFor && <Text>Looking for {formatLookingFor(studentProfile.lookingFor)}</Text>}
+              <br />
+              {studentProfile.graduationDate && (
+                <Text>Graduating in {format(studentProfile.graduationDate, "MMMM, yyyy")}</Text>
+              )}
+            </Box>
 
-          {studentProfile.lookingFor && <Text>Looking for {formatLookingFor(studentProfile.lookingFor)}</Text>}
+            <Flex align="center" gap="2">
+              <BsEnvelope />
+              <Link href={`mailto:${studentProfile.user.email}`}>{studentProfile.user.email}</Link>
+            </Flex>
 
-          {studentProfile.graduationDate && (
-            <Text>Graduating in {format(studentProfile.graduationDate, "MMMM, yyyy")}</Text>
-          )}
-
-          <Flex align="center" gap="2">
-            <BsEnvelope />
-            <Link href={`mailto:${studentProfile.user.email}`}>{studentProfile.user.email}</Link>
-          </Flex>
-
-          <Flex gap="2">
-            {studentProfile.personalWebsite && (
-              <Flex align="center" gap="2">
-                <Link href={studentProfile.personalWebsite} target="_blank">
-                  <BsGlobe size="25" />
-                </Link>
-              </Flex>
-            )}
-
-            {studentProfile.github && (
-              <Flex align="center" gap="2">
-                <Link href={studentProfile.github} target="_blank">
-                  <BsGithub size="25" />
-                </Link>
-              </Flex>
-            )}
-
-            {studentProfile.linkedIn && (
-              <Flex align="center" gap="2">
-                <Link href={studentProfile.linkedIn} target="_blank">
-                  <BsLinkedin size="25" />
-                </Link>
-              </Flex>
-            )}
-          </Flex>
-        </Flex>
-
-        <Flex>
-          <Flex direction="column" gap="3">
-            {studentProfile.bio && (
-              <Flex direction="column" gap="3">
-                <Heading size="5">About me</Heading>
-                <Markdown>{studentProfile.bio}</Markdown>
-              </Flex>
-            )}
-
-            {studentProfile.skills && (
-              <Flex direction="column" gap="3">
-                <Heading size="5">Skills</Heading>
-                <Flex>
-                  {studentProfile.skills.map((skill, id) => (
-                    <Chip label={skill} key={id} />
-                  ))}
+            <Flex gap="2">
+              {studentProfile.personalWebsite && (
+                <Flex align="center" gap="2">
+                  <Link href={studentProfile.personalWebsite} target="_blank">
+                    <BsGlobe size="25" color="black" />
+                  </Link>
                 </Flex>
-              </Flex>
-            )}
+              )}
 
-            {studentProfile.interests && (
-              <Flex direction="column" gap="3">
-                <Heading size="5">Interests</Heading>
-                <Flex>
-                  {studentProfile.interests.map((interest, id) => (
-                    <Chip label={interest} key={id} />
-                  ))}
+              {studentProfile.github && (
+                <Flex align="center" gap="2">
+                  <Link href={studentProfile.github} target="_blank">
+                    <BsGithub size="25" color="black" />
+                  </Link>
                 </Flex>
-              </Flex>
-            )}
+              )}
+
+              {studentProfile.linkedIn && (
+                <Flex align="center" gap="2">
+                  <Link href={studentProfile.linkedIn} target="_blank">
+                    <BsLinkedin size="25" color="black" />
+                  </Link>
+                </Flex>
+              )}
+            </Flex>
           </Flex>
-        </Flex>
+        </Card>
+
+        <Card className={styles.longDetailsCard}>
+          <Flex>
+            <Flex direction="column" gap="3">
+              {studentProfile.bio && (
+                <Flex direction="column" gap="3">
+                  <Heading size="5">About me</Heading>
+                  <Markdown>{studentProfile.bio}</Markdown>
+                </Flex>
+              )}
+
+              {studentProfile.skills && (
+                <Flex direction="column" gap="3">
+                  <Heading size="5">Skills</Heading>
+                  <Flex gap="1">
+                    {studentProfile.skills.map((skill, id) => (
+                      <Chip label={skill} key={id} />
+                    ))}
+                  </Flex>
+                </Flex>
+              )}
+
+              {studentProfile.interests && (
+                <Flex direction="column" gap="3">
+                  <Heading size="5">Interests</Heading>
+                  <Flex gap="1">
+                    {studentProfile.interests.map((interest, id) => (
+                      <Chip label={interest} key={id} />
+                    ))}
+                  </Flex>
+                </Flex>
+              )}
+            </Flex>
+          </Flex>
+        </Card>
       </Flex>
     </StudentOnlyArea>
   )
