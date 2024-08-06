251bfc8e0314092a94b84343c0b299f74daacebb
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-08-06 12:09:05 +0100

    chore: format document

--- lib/graph.ts
+++ lib/graph.ts
@@ -3,18 +3,18 @@
  */
 
 export async function makeGraphRequest(access_token: string, endpoint: string) {
-	return fetch(`https://graph.microsoft.com/v1.0/${endpoint}`, {
-		headers: { Authorization: `Bearer ${access_token}` },
-	})
+  return fetch(`https://graph.microsoft.com/v1.0/${endpoint}`, {
+    headers: { Authorization: `Bearer ${access_token}` },
+  })
 }
 
 export async function getDepartment(access_token: string) {
-	const response = await makeGraphRequest(access_token, "/me?$select=department")
-	try {
-		const data = await response.json()
-		return data.department
-	} catch {
-		console.error("Error fetching department")
-		return null
-	}
+  const response = await makeGraphRequest(access_token, "/me?$select=department")
+  try {
+    const data = await response.json()
+    return data.department
+  } catch {
+    console.error("Error fetching department")
+    return null
+  }
 }
