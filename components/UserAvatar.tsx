36de1658424fd8ccfd4fd88adbe4a4f45ce982a8
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2024-08-07 11:00:13 +0100

    fix: responsive text size for user avatar component
    Co-authored-by: Matthew Alex <matalex412@gmail.com>

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -1,22 +1,16 @@
 import styles from "@/components/userAvatar.module.scss"
 
 import { Avatar } from "@radix-ui/themes"
-import { Responsive } from "@radix-ui/themes/props"
 import { User } from "next-auth"
 import React from "react"
 
-const UserAvatar = ({
-  user,
-  size,
-}: {
-  user: User
-  size: Responsive<"9" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8">
-}) => (
+const UserAvatar = ({ user, size }: { user: User; size: "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" }) => (
   <Avatar
     src={"/api/ms-graph/profile" ?? undefined}
     alt="Profile"
     radius="full"
     size={size}
+    style={{ fontSize: `${parseInt(size) / 3.5}rem` }}
     className={styles.Avatar}
     fallback={
       user.name
