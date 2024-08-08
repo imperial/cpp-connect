cbe0db011b1706813b5e4cea9c61aed0bcb50510
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2024-08-08 16:40:48 +0100

    feat: fill header card

--- app/opportunities/[id]/page.tsx
+++ app/opportunities/[id]/page.tsx
@@ -1,11 +1,14 @@
+import Chip from "@/components/Chip"
 import prisma from "@/lib/db"
 
 import styles from "./page.module.scss"
 
-import { Card, Flex, Inset } from "@radix-ui/themes"
+import { Box, Button, Card, Flex, Heading, Inset } from "@radix-ui/themes"
 import Image from "next/image"
+import Link from "next/link"
 import { notFound } from "next/navigation"
 import React from "react"
+import { BsBoxArrowUpRight } from "react-icons/bs"
 
 const OpportunityPage = async ({ params }: { params: { id: string } }) => {
   const opportunity = await prisma.opportunity.findUnique({
@@ -29,7 +32,30 @@ const OpportunityPage = async ({ params }: { params: { id: string } }) => {
           />
         </Inset>
 
-        <Flex wrap="wrap" direction="column"></Flex>
+        <Flex wrap="wrap" direction="column" p="5" gap="5">
+          <Box>
+            <Heading size="8">{opportunity.position}</Heading>
+
+            <Box>
+              At{" "}
+              <Link href={`/companies/${encodeURIComponent(opportunity.company.name)}`}>
+                {opportunity.company.name}
+              </Link>
+            </Box>
+          </Box>
+
+          <Flex gap="1" wrap="wrap">
+            <Chip label={opportunity.location} />
+            <Chip label={opportunity.type} />
+          </Flex>
+
+          <Button asChild size="4" className={styles.applyButton}>
+            <Link href={opportunity.link} target="_blank">
+              Apply Now
+              <BsBoxArrowUpRight />
+            </Link>
+          </Button>
+        </Flex>
       </Card>
 
       <Card className={styles.aboutCard}></Card>
