b72f1c256a48fc4dcad046be83bacdce3ee84c2d
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-08-09 10:33:43 +0100

    feat: new RestrictedAreaCompany component to restrict by company ID
    Co-authored-by: IlliaDerevianko <IlliaDerevianko@users.noreply.github.com>

--- app/companies/[id]/CompanyManagement.tsx
+++ app/companies/[id]/CompanyManagement.tsx
@@ -1,7 +1,5 @@
-"use client"
-
 import { AddUser } from "@/app/companies/[id]/AddUser"
-import TanstackTable from "@/components/TanstackTable"
+import RestrictedAreaCompany from "@/components/rbac/RestrictedAreaCompany"
 
 import UsersTable, { UsersTableRow } from "./UsersTable"
 import styles from "./company-management.module.scss"
@@ -12,17 +10,19 @@ import React from "react"
 
 export const CompanyManagement = ({ company }: { company: CompanyProfile & { companyUsers?: UsersTableRow[] } }) => {
   return (
-    <Card className={styles.companyManagerCard}>
-      <Flex direction="column" gap="3" p="4">
-        <Flex gap="3" direction="row" align="center" justify="between">
-          <Heading size="6">Company Management</Heading>
-          <AddUser company={company} />
-        </Flex>
+    <RestrictedAreaCompany companyId={company.id} showMessage={false}>
+      <Card className={styles.companyManagerCard}>
+        <Flex direction="column" gap="3" p="4">
+          <Flex gap="3" direction="row" align="center" justify="between">
+            <Heading size="6">Company Management</Heading>
+            <AddUser company={company} />
+          </Flex>
 
-        <Box pl="9" pr="9">
-          <UsersTable users={company.companyUsers} />
-        </Box>
-      </Flex>
-    </Card>
+          <Box pl="9" pr="9">
+            <UsersTable users={company.companyUsers} />
+          </Box>
+        </Flex>
+      </Card>
+    </RestrictedAreaCompany>
   )
 }
