51a8a123ce25b86a7ceac9eb3382144ae63a9141
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-08-13 15:34:14 +0100

    fix: issue where server would error 500 on companies page as window not defined

--- app/companies/slug.ts
+++ app/companies/slug.ts
@@ -1,2 +1,4 @@
-export const SLUG_START = "https://" + window.location.host + "/companies/"
+/** NOTE: DO NOT USE IN SERVER COMPONENT AS WILL NOT RENDER PROPERLY ON SERVER */
+export const SLUG_START =
+  "https://" + (typeof window !== "undefined" ? window?.location?.host : "cpp-connect") + "/companies/"
 export const slugComputer = (companyName: string) => companyName.toLowerCase().replace(/\s/g, "-")
