fad555645cb6575e7d7f8b61a68ad4d114394ee6
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-08-14 15:26:52 +0100

    wip: second draft of login page scaffold

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,21 +2,29 @@ import Link from "@/components/Link"
 
 import styles from "./page.module.scss"
 
-import { Button, Flex, Separator } from "@radix-ui/themes"
+import { Button, Flex, Separator, Text } from "@radix-ui/themes"
+import { Heading } from "@react-email/components"
 import Image from "next/image"
 import React from "react"
 
 const LoginPage = async () => {
   return (
-    <Flex gap="5" direction="column">
+    <Flex gap="6" direction="column">
       <Flex className={styles.logosContainer}>
         <Image src="/images/imperial-logo-blue.svg" alt="imperial logo in blue" width={0} height={0} />
       </Flex>
-      <Flex pl="9" pr="9" direction="column" gap="4">
-        <Button size="3">Login with SSO</Button>
-        <Button size="3" asChild>
-          <Link href="/auth/login/partner">Partner Sign In</Link>
-        </Button>
+      <Flex pl="9" pr="9" direction="column" gap="5">
+        <Flex direction="column" justify="center" align="center">
+          <Heading as="h1">CPP Connect</Heading>
+          <Text>Giving you a head start in your career</Text>
+        </Flex>
+        <Separator size="4" />
+        <Flex direction="column" gap="3">
+          <Button size="3">Login with SSO</Button>
+          <Link href="/auth/login/partner" style={{ textAlign: "center" }}>
+            Industry Partner?
+          </Link>
+        </Flex>
       </Flex>
     </Flex>
   )
