865d78721a9994feadbed840d085fcd9cc4913cc
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-08-14 15:37:05 +0100

    feat: wire-up login button

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,13 +1,38 @@
+import { signIn } from "@/auth"
 import Link from "@/components/Link"
 
 import styles from "./page.module.scss"
 
 import { Button, Flex, Separator, Text } from "@radix-ui/themes"
 import { Heading } from "@react-email/components"
+import { AuthError } from "next-auth"
 import Image from "next/image"
+import { redirect } from "next/navigation"
 import React from "react"
 
 const LoginPage = async () => {
+  const signInEntraID = async () => {
+    "use server"
+    try {
+      await signIn("microsoft-entra-id", {
+        redirectTo: "/",
+      })
+    } catch (error) {
+      // Signin can fail for a number of reasons, such as the user
+      // not existing, or the user not having the correct role.
+      // In some cases, you may want to redirect to a custom error
+      if (error instanceof AuthError) {
+        return redirect(`/auth/errror?error=${error.type}`)
+      }
+
+      // Otherwise if a redirects happens NextJS can handle it
+      // so you can just re-thrown the error and let NextJS handle it.
+      // Docs:
+      // https://nextjs.org/docs/app/api-reference/functions/redirect#server-component
+      throw error
+    }
+  }
+
   return (
     <Flex gap="6" direction="column">
       <Flex className={styles.logosContainer}>
@@ -20,7 +45,11 @@ const LoginPage = async () => {
         </Flex>
         <Separator size="4" />
         <Flex direction="column" gap="3">
-          <Button size="3">Login with SSO</Button>
+          <form action={signInEntraID} style={{ width: "100%" }}>
+            <Button size="3" style={{ width: "100%" }}>
+              Login with SSO
+            </Button>
+          </form>
           <Link href="/auth/login/partner" style={{ textAlign: "center" }}>
             Industry Partner?
           </Link>
