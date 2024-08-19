8ae3d726b62106fcc0c21782d6356bcdf310fae3
Illia Derevianko <iliaderevianko30@gmail.com>
2024-08-19 12:43:32 +0300

    chore: remove unused imports and add the missing ones

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,6 @@ import "@/styling/globals.scss"
 
 import styles from "./page.module.scss"
 
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker"
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo"
 import * as React from "react"
 
 const Home = async () => {
