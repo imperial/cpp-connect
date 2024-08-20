ab900fa590672135cf84f5603cb6be24f32bacc6
Matthew Alex <matalex412@gmail.com>
2024-08-20 15:07:09 +0100

    fix: remove superfluous type intersection
    Co-authored-by: Alexander Biraben-Renard <ab1223@ic.ac.uk>

--- app/opportunities/OpportunityTable.tsx
+++ app/opportunities/OpportunityTable.tsx
@@ -14,7 +14,7 @@ import { useMemo } from "react"
 
 type OpportunityRow = {
   company: CompanyProfile
-} & Opportunity & { adminButtons?: void }
+} & Opportunity
 
 type ColumnName = keyof OpportunityRow | `company.${keyof CompanyProfile}`
 
