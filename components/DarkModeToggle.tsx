c7882301db49b038d2e926cd0d6c9c683aae768a
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2024-08-22 10:31:50 +0100

    revert: remove page theme change on system theme change

--- components/DarkModeToggle.tsx
+++ components/DarkModeToggle.tsx
@@ -2,29 +2,10 @@
 
 import { IconButton, Tooltip } from "@radix-ui/themes"
 import { useTheme } from "next-themes"
-import { useEffect } from "react"
 import { BsFillMoonFill, BsSunFill } from "react-icons/bs"
-import { useMediaQuery } from "react-responsive"
-
-let previousSystemTheme: boolean
 
 const DarkModeToggle = ({ fill = "currentColor" }: { fill?: string }) => {
   const { theme, setTheme } = useTheme()
-
-  const prefersDarkMode = useMediaQuery({ query: "(prefers-color-scheme: dark)" })
-
-  // Save the initial system theme
-  useEffect(() => {
-    previousSystemTheme = prefersDarkMode
-  }, [])
-
-  // Update the theme when the system theme actively changes while the user is on the page
-  useEffect(() => {
-    if (previousSystemTheme !== prefersDarkMode) {
-      previousSystemTheme = prefersDarkMode
-      setTheme(prefersDarkMode ? "dark" : "light")
-    }
-  }, [prefersDarkMode, setTheme])
 
   return (
     <Tooltip content={`Switch to ${theme === "dark" ? "light" : "dark"} mode`}>
