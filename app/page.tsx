af620276f55bf61c97ec1a426b9fce03b0c3d2ef
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2024-08-23 11:40:01 +0100

    feat: add slide card component and landing page skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import SlideCard from "@/components/SlideCard"
 import "@/styling/globals.scss"
 
 import styles from "./page.module.scss"
@@ -5,7 +6,22 @@ import styles from "./page.module.scss"
 import * as React from "react"
 
 const Home = async () => {
-  return <main className={styles.main}></main>
+  const titles = [
+    "Welcome to CPP Connect",
+    "Connecting quality Imperial students with quality employers",
+    "Find your next opportunity",
+    "Join today",
+  ]
+
+  return (
+    <main className={styles.main}>
+      {titles.map((title, index) => (
+        <SlideCard key={index} className={styles.slideCard} direction={index % 2 === 0 ? "left" : "right"}>
+          <h1>{title}</h1>
+        </SlideCard>
+      ))}
+    </main>
+  )
 }
 
 export const dynamic = "force-dynamic"
---
+++ components/SlideCard.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { Card, CardProps } from "@radix-ui/themes"
+import { useInView } from "framer-motion"
+import React, { useRef } from "react"
+
+const SlideCard = (props: CardProps & React.RefAttributes<HTMLDivElement> & { direction?: "left" | "right" }) => {
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
+
+  return (
+    <Card
+      {...props}
+      ref={ref}
+      style={{
+        transform: isInView ? "none" : props.direction === "left" ? "translateX(-200px)" : "translateX(200px)",
+        opacity: isInView ? 1 : 0,
+        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+      }}
+    />
+  )
+}
+
+export default SlideCard
